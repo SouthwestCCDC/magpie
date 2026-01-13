7982f041c7e535db875e12e474e9158d401c5ca7
George Louthan <duplico@dupli.co>
2026-01-13 16:32:56 -0600

    fix: Remove duplicate cli_runner fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -161,12 +161,6 @@ def cli_runner() -> CliRunner:
 
 
 @pytest.fixture
-def cli_runner() -> CliRunner:
-    """Create Click CLI test runner."""
-    return CliRunner()
-
-
-@pytest.fixture
 def cli_runner_no_config() -> CliRunner:
     """Create CLI runner that simulates no server/token configuration.
 
